352443878c8754e61775a63bb4a691a4a9d29f0e
Jay Ostinowsky <dukebdfan@comcast.net>
2012-05-02 15:24:13 -0400

    Update

--- GoldStarDAL/goldstar.py
+++ GoldStarDAL/goldstar.py
@@ -1,16 +1,17 @@
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///:memory:', echo = False)
-Base = declarative_base()
-class User(Base):
+userEngine = create_engine('sqlite:///:memory:', echo = False)
+userBase = declarative_base()
+starBase = declarative_base()
+class User(userBase):
 	__tablename__ = 'users'
 
 	id = Column(Integer, Sequence('user_id_seq'), primary_key = True)
 	firstName = Column(String(50))
 	lastName = Column(String(50))
 	email = Column(String(100))
-	
+
 	def __init__(self, firstname, lastname, email):
 		self.firstName = firstname
 		self.lastName = lastname
@@ -18,7 +19,13 @@ class User(Base):
 	def __repr__(self):
 		return "<User('%s', '%s' , '%s')>" % (self.firstName, self.lastName, self.email)
 
-Base.metadata.create_all(engine)
+class Star(starBase):
+	__tablename__ = 'star'
+
+	id = Column(Integer, Sequence('star_id_seq'), primary_key = True)
+	
+
+userBase.metadata.create_all(userEngine)
 def addusertodatabase(firstnamefromhtml, lastnamefromhtml, emailfromhtml):
 	success = False
 	if len(firstnamefromhtml) > 50 or len(lastnamefromhtml) > 50 or len(emailfromhtml) > 100:
@@ -27,7 +34,7 @@ def addusertodatabase(firstnamefromhtml, lastnamefromhtml, emailfromhtml):
 		emailfromhtml = emailfromhtml[:100]
 	if len(firstnamefromhtml) != 0 and lastnamefromhtml != 0 and emailfromhtml != 0:
 		if firstnamefromhtml.isalpha() and lastnamefromhtml.isalpha():
-			Session = sessionmaker(bind = engine)
+			Session = sessionmaker(bind = userEngine)
 			session = Session()
 			newUser = User(firstnamefromhtml, lastnamefromhtml, emailfromhtml)
 			session.add(newUser)
@@ -35,7 +42,7 @@ def addusertodatabase(firstnamefromhtml, lastnamefromhtml, emailfromhtml):
 			success = True
 	return success
 def listtheusers():
-	Session = sessionmaker(bind = engine)
+	Session = sessionmaker(bind = userEngine)
 	session = Session()
 	for instance in session.query(User).order_by(User.firstName):
 		print instance.firstName
