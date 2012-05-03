fd688a24dd663b6a122ab934a1bbfd92e73d7553
Jay Ostinowsky <dukebdfan@comcast.net>
2012-05-03 14:54:03 -0400

    Flask Restless Version

--- app.py
+++ app.py
@@ -2,18 +2,13 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import flask.ext.restless
 
-# Create the Flask application and the Flask-SQLAlchemy object.
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-# Create your Flask-SQLALchemy models as usual but with the following two
-# (reasonable) restrictions:
-#   1. They must have an id column of type Integer.
-#   2. They must have an __init__ method which accepts keyword arguments for
-#      all columns (the constructor in flask.ext.sqlalchemy.SQLAlchemy.Model
-#      supplies such a method, so you don't need to declare a new one).
+
 
 class Star(db.Model):
 
@@ -39,14 +34,13 @@ class User(db.Model):
 		return "<User('%s', '%s' , '%s')>" % (self.firstName, self.lastName, self.email)
 
 
-# Create the database tables.
+
 db.create_all()
 
-# Create the Flask-Restless API manager.
+
 manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
 
-# Create API endpoints, which will be available at /api/<tablename> by
-# default. Allowed HTTP methods can be specified as well.
+
 manager.create_api(User, methods=['GET', 'POST', 'DELETE'])
 manager.create_api(Star, methods=['GET', 'POST', 'DELETE'])
 
