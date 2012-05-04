036f41fe9c572fe5f3957712a9a843a612ad6f18
Jay Ostinowsky <dukebdfan@comcast.net>
2012-05-04 14:07:23 -0400

    updated test.py

--- test.py
+++ test.py
@@ -41,6 +41,46 @@ class testdb(unittest.TestCase):
 		owner = User.query.get(2)
 		print issuer.issued
 		print owner.stars
+	def test_save_stars_more_than_one(self):
+		u = User()
+		u.firstName=u"Walt"
+		u.lastName=u"Grata"
+		u.email=u"wegrata@gmail.com"
+		u1 = User()
+		u1.firstName=u"John"
+		u1.lastName=u"Smith"
+		u1.email=u"jsmith@gmail.com"
+		u2 = User()
+		u2.firstName = u"Jay"
+		u2.lastName = u"Ostinowsky"
+		u2.email = u"dukebdfan@comcast.net"
+		db.session.add(u)
+		db.session.add(u1)
+		db.session.add(u2)
+		db.session.commit()
+		A = User.query.get(1)
+		B = User.query.get(2)
+		C = User.query.get(3)
+		s1 = Star()
+		s1.category = u"INFLUENCED"
+		s1.description = u"This is a test"
+		s2 = Star()
+		s2.category = u"HELPED"
+		s2.description = u"THIS IS ANOTHER TEST"
+		s1.issuer_id = A.id
+		s1.owner_id = B.id
+		s2.owner_id = A.id
+		s2.issuer_id = C.id
+		db.session.add(s1)
+		db.session.add(s2)
+		db.session.commit()
+		A = User.query.get(1)
+		B = User.query.get(2)
+		C = User.query.get(3)
+		print A.stars
+		print C.issued
+		print B.stars
+		print A.issued
 
 
 
