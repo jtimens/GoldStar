ffa231900f948e57a2ace8cf08b7825c4d9cbc32
Walt Grata <wegrata@gmail.com>
2012-05-08 10:12:01 -0400

    setup for heroku

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import flask.ext.restless
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zoubpmfsdtxwoq:3G0ELHUf2BcAOSF1hUxDceKsQL@ec2-23-23-234-187.compute-1.amazonaws.com/resource44881'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sql'
 db = SQLAlchemy(app)
 
 
