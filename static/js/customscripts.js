89b483ae265a2bf969d49d34f1050d26e836996e
PS Intern <PS@PS-PC.(none)>
2012-05-21 14:39:37 -0400

    changed customscripts.js form

--- static/js/customscripts.js
+++ static/js/customscripts.js
@@ -187,7 +187,7 @@ function postJSON(id, num)
 		$.getJSON(URL, function(jdata){
 			if(jdata.objects.length)
 				alert('You have successfully created an account!');
-				toggleLoginView("login2");
+				toggleLoginView("login1");
 		});
   		//alert('You have successfully created an account!');
 	}
