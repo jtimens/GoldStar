42b4468cec4e3fba25de0e01add196f66a46b237
Mark Dewey <mdewey@problemsolutions.net>
2012-06-04 14:29:03 -0400

    Finished log in and sign up pages

---
+++ static/js/passwordValidator.js
@@ -0,0 +1,71 @@
+
+function sendUserInfoToServer()
+{
+
+	
+		var noerr = true;
+		var fn = $("#FName").val(); 
+		var ln = $("#LName").val(); 
+		var em = $("#Email").val();
+		//validate password
+		var pw = $("#PWord").val();
+		var confirmedPassword = $("#PWordAgain").val();
+		if (pw == confirmedPassword)
+		{
+				//send to Server and sign up
+				//lowercase email address
+			em = em.toLowerCase();
+
+			var userData = '{"firstName":"'+fn+'","lastName":"'+ln+'","email":"'+em+'", "password":"'+pw+'"}';
+			var myJSON = userData;
+			var URL = "/api/user";
+			$.ajax({
+	 			type: 'POST',
+	  			url: URL,
+	  			data: myJSON,
+	  			contentType: "application/json",
+				success: function(data, textStatus, jqXHR){
+					sessionStorage.userID = data.id;
+					alert('You have successfully created an account!');
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					var err = jQuery.parseJSON(jqXHR.responseText);
+					if(err.validation_errors.firstName)
+						alert(err.validation_errors.firstName);
+					else if(err.validation_errors.lastName)
+						alert(err.validation_errors.lastName);
+					else if(err.validation_errors.email)
+						alert(err.validation_errors.email);
+					noerr = false;
+				},
+	  			complete: function(jdata){
+	  				if (noerr == true){
+	  					//login();
+	  					myJSON = '{"email":"'+em+'", "password":"'+pw+'"}'
+	  					$.ajax({
+							type: 'POST',
+							url: '/login',
+							data: myJSON,
+							contentType: "application/json",
+							success:function(data, textStatus,jqXHR){
+								sessionStorage.userID = data.id;
+								login();
+							}
+						});
+	  				}
+	  				else{
+	  					toggleLoginView("login2");
+	  				}
+					
+	  			}
+			});
+		
+		}
+		else
+		{
+			///tell user that passwords do not match
+			alert("Passwords do not match, please try again.");
+
+		}
+		
+}
