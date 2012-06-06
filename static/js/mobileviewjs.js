c58efd3bae5d15ac5d82dcc48aad9d6188c57b68
Mark Dewey <mdewey@problemsolutions.net>
2012-06-06 15:35:48 -0400

    commiting to pull down

--- static/js/mobileviewjs.js
+++ static/js/mobileviewjs.js
@@ -310,7 +310,9 @@ function displayMyStars()
 				var mm = today.getMonth()+1; //January is 0!
 
 				var yyyy = today.getFullYear();
-				if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} var today = mm+'/'+dd+'/'+yyyy;
+				if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} 
+				var atTime = today.getHours() +":"+today.getMinutes() ;
+				var today = mm+'/'+dd+'/'+yyyy + " @" +atTime;
 				console.log(today);
 				var itemHTML = getItemHTML(ownerID, ownerName, verb, issuerID, issuerName, hashtag, today);
 				$("#myStarList").append(itemHTML);	
